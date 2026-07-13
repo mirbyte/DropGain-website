0b03773eb61da14f85875c351c5eaa1673d185fb
mirbyte <83219244+mirbyte@users.noreply.github.com>
2026-07-13 18:55:40 +0300

    Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://dropgain.app',
   base: '/',
   output: 'static',
+  integrations: [sitemap()],
   image: {
     service: passthroughImageService(),
   },
